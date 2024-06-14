b8896d57c73fb8699194cb82c5cde7d16a9acf3c
jonnyelisha <jonathanelisha98@gmail.com>
2024-06-14 08:44:36 +0300

    Update ExercisesXP__.py

--- Week 1/Day4/ExercisesXP__.py
+++ Week 1/Day4/ExercisesXP__.py
@@ -1,77 +1,72 @@
-####
-###Exercise1
-##def display_message():
-##    print("This is how functions work")
-##
-##display_message()
-####
-##
-##
-###Exercise2
-##def favorite_book(title):
-##        print(f"One of my favorite books is {title}")
-##    
-##favorite_book("Alice in Wonderland")
-##
-##
-###Exercise3
-##
-##def describe_city(city, country = "USA"):
-##    print(f"{city} is in {country}")
-##
-##
-##describe_city("reere")
-##describe_city("Kentucky", "France")
-##
-##
-##
-###Exercise4
-##import random
-##
-##
-##def number_acceptance():
-##    num1 = int(input(f"Give me a number from 1-100: "))
-##    if 1< num1 < 100:
-##        rand_num= random.randint(1,100)
-##        if num1 == rand_num:
-##            print("Success")
-##        else:
-##            print(f"Failure {num1}, {rand_num}")
-##    else:
-##        print("Choose another number")
-##
-##
-##number_acceptance()
+
+Exercise1
+def display_message():
+    print("This is how functions work")
+display_message()
 
 
-##size = {S,M,L}
-##
-##
-##
+
+Exercise2
+def favorite_book(title):
+        print(f"One of my favorite books is {title}")
+    
+favorite_book("Alice in Wonderland")
 
 
-#Exercise 5
-##def make_shirt(size = "S",text = "I love Python"):
-##    print(f"Your size is {size} with it saying {text}")
-##
-##make_shirt("Large")
-##make_shirt("Medium")
-##make_shirt("Medium", "Shit")
+Exercise3
+
+def describe_city(city, country = "USA"):
+    print(f"{city} is in {country}")
+
+describe_city("reere")
+describe_city("Kentucky", "France")
+
+Exercise4
+import random
+
+
+def number_acceptance():
+    num1 = int(input(f"Give me a number from 1-100: "))
+    if 1< num1 < 100:
+        rand_num= random.randint(1,100)
+        if num1 == rand_num:
+            print("Success")
+        else:
+            print(f"Failure {num1}, {rand_num}")
+    else:
+        print("Choose another number")
+
+
+number_acceptance()
+
+
+size = {S,M,L}
 
 
 
 
-#Exercise#6
-##magicians_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
-##
-##def show_magicians(magician_names):
-##    for magician in magician_names:
-##        print(f"{magician}")
-##
-##
-##show_magicians(magicians_names)
 
-##
+Exercise 5
+def make_shirt(size = "S",text = "I love Python"):
+    print(f"Your size is {size} with it saying {text}")
+
+make_shirt("Large")
+make_shirt("Medium")
+make_shirt("Medium", "Stuff")
+
+
+
+
+Exercise#6
+magicians_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+def show_magicians(magician_names):
+    for magician in magician_names:
+        print(f"{magician}")
+
+show_magicians(magicians_names)
+
+
 
 
 
