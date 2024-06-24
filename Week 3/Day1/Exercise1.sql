9993be96d7a78e545d96542570e586f332929105
jonnyelisha <jonathanelisha98@gmail.com>
2024-06-24 12:34:35 +0300

    Update Exercise1.sql

--- Week 3/Day1/Exercise1.sql
+++ Week 3/Day1/Exercise1.sql
@@ -1,1 +1,37 @@
+
+
+create table items(
+    id serial primary key,
+    name varchar(50) Not null,
+    price integer not null
+)
+
+
+create table customers(
+    id serial primary key,
+    first_name varchar(100) not null,
+    last_name varchar(100) not null
+)
+
+insert into items(name,price) values ('Small Desk', 100)
+insert into items(name,price) values ('Large Desk', 300)
+insert into items(name,price) values ('Fan', 80)
+
+
+#Adding 5 customers to customers table
+insert into customers (first_name, last_name), values ('Greg', 'Jones');
+insert into customers (first_name, last_name), values ('Sandra', 'Jones');
+insert into customers (first_name, last_name), values ('Scott', 'Scott');
+insert into customers (first_name, last_name), values ('Trevor', 'Green')
+insert into customers (first_name, last_name), values ('Melanie' 'Johnson');
+
+
+
+
+#Fetching data from the database 
+
+select * from items where price > 80;
+select * from price where price <= 300 ;
+select * from customers where last_name = 'Smith';
+select * from customers where last_name = 'Jones';
 select * from customers where first_name !='Scott';
