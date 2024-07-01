23ca6c4427c18447971ea5271a7d425e44c374c0
jonnyelisha <jonathanelisha98@gmail.com>
2024-07-01 16:28:19 +0300

    Add files via upload

---
+++ Week 2/Day5/RemoteLearning.py
@@ -0,0 +1,9 @@
+class Human:
+    def __init__(self,id_number, name, age, priority, blood_type):
+        self.id_number = id_number
+        self.name =  name
+        self.age = age
+        self.priority = priority
+        self.blood_type =  blood_type
+
+        
