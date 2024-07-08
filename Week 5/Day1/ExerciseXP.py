e02f40b38147f3ab31003060473fc20d80ce9551
jonnyelisha <jonathanelisha98@gmail.com>
2024-07-08 11:13:21 +0300

    Add files via upload

---
+++ Week 5/Day1/ExerciseXP.py
@@ -0,0 +1,5 @@
+#Exercise1: Introduction to Data Analysis
+# Data Analysis - the process of working with data to find important information
+# important because it gives valuable information about the data set you are working with
+
+#Exercise 2: DataSet loading and initial analysis
